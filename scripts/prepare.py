66bc61db0d168c05c4ce6c3db8d0d15f49b1a372
Pol Monroig <hello@polmonroig.com>
2024-03-30 12:30:54 +0100

    Added pandas output to prepare

--- scripts/prepare.py
+++ scripts/prepare.py
@@ -2,6 +2,7 @@ import sys
 sys.path.insert(0, '..')
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
 from modules import constants
 import pandas as pd
 import pickle
@@ -18,7 +19,7 @@ if __name__ == '__main__':
     soil_vars = list(all_cols[all_cols.str.startswith('SOIL')])
     modis_vars = list(all_cols[all_cols.str.startswith('MODIS')])
     vod_vars = list(all_cols[all_cols.str.startswith('VOD')])
-    train_vars = world_climate_vars + soil_vars + modis_vars + vod_vars 
+    train_vars = ['id'] + world_climate_vars + soil_vars + modis_vars + vod_vars 
     target_vars = list(all_cols[all_cols.str.endswith('_mean')])
     # dataset split
     print('Splitting data...')
@@ -26,26 +27,31 @@ if __name__ == '__main__':
     y = train_metadata[target_vars]
     X_test = X_test[train_vars]
     X_train, X_val, y_train, y_val =  train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    
+
     # scaler definition 
     print('Preparing data...')
-    X_scaler = StandardScaler()
-    y_scaler = StandardScaler()
-    X_scaler.fit(X_train)
-    y_scaler.fit(y_train)
+    X_transformer = ColumnTransformer([
+        ("standarize", StandardScaler(), train_vars[1:])
+    ], remainder='passthrough')
+    y_transformer = ColumnTransformer([
+        ("standarize", StandardScaler(), target_vars)
+    ])
+    X_transformer.set_output(transform = 'pandas')
+    y_transformer.set_output(transform = 'pandas')
+    X_transformer.fit(X_train)
+    y_transformer.fit(y_train)
     print('Saving scalers...')
-    with open(os.path.join(constants.model_path, 'X_scaler.pickle'), 'wb') as f:
-        pickle.dump(X_scaler, f)
-    with open(os.path.join(constants.model_path, 'y_scaler.pickle'), 'wb') as f:
-        pickle.dump(y_scaler, f)
+    with open(os.path.join(constants.model_path, 'X_transformer.pickle'), 'wb') as f:
+        pickle.dump(X_transformer, f)
+    with open(os.path.join(constants.model_path, 'y_transformer.pickle'), 'wb') as f:
+        pickle.dump(y_transformer, f)
     
     # transformations
-    X_train = pd.DataFrame(X_scaler.transform(X_train))
-    X_val = pd.DataFrame(X_scaler.transform(X_val))
-    X_test = pd.DataFrame(X_scaler.transform(X_test))
-    y_train = pd.DataFrame(y_scaler.transform(y_train))
-    y_val = pd.DataFrame(y_scaler.transform(y_val))
+    X_train = pd.DataFrame(X_transformer.transform(X_train))
+    X_val = pd.DataFrame(X_transformer.transform(X_val))
+    X_test = pd.DataFrame(X_transformer.transform(X_test))
+    y_train = pd.DataFrame(y_transformer.transform(y_train))
+    y_val = pd.DataFrame(y_transformer.transform(y_val))
 
     # saving
     print('Saving...')
